81f8109f047d1cd61dff9afcc155ab8d92191774
yashraj-pradip-haridas <yashraj.haridas22@pccoepune.org>
2026-03-17 00:47:57 +0530

    implemented the directory level recursive search

--- Personal_projects/mini_grep/src/main.rs
+++ Personal_projects/mini_grep/src/main.rs
@@ -1,13 +1,13 @@
 use colored::Colorize;
 use std::{
     env,
-    fs::File,
-    io::{BufRead, BufReader},
+    fs::{self, File},
+    io::{self, BufRead, BufReader},
     path::Path,
 };
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // println!("{:?}", args);
+    println!("{:?}", args);
     let pattern = match args.get(1) {
         Some(pattern) => {
             let pattern = pattern.to_lowercase();
@@ -36,13 +36,44 @@ fn main() {
         println!("{}", "Invalid path / File does not exist".red());
         return;
     }
-    search_file(file_path, pattern);
+    println!(
+        "{:>20} {:>8}  {}",
+        "File Path".yellow(),
+        "Line No.".yellow(),
+        "Text".yellow()
+    );
+    if file_path.is_file() {
+        search_file(file_path, &pattern);
+    } else if file_path.is_dir() {
+        match search_directory(file_path, &pattern) {
+            Ok(_) => println!("Success"),
+            Err(_) => println!("Failed"),
+        };
+    }
 }
 
+fn search_directory(file_path: &Path, pattern: &String) -> io::Result<()> {
+    for entry in fs::read_dir(file_path)? {
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(_) => {
+                "Failed to access directory";
+                continue;
+            }
+        };
+        let path = entry.path();
+        if path.is_file() {
+            // println!("{:?} - inside search directory", path.display());
+            search_file(&path, &pattern);
+        } else if path.is_dir() {
+            let _ = search_directory(file_path, pattern);
+        }
+    }
+    Ok(())
+}
 
-
-
-fn search_file(file_path: &Path, pattern: String) {
+fn search_file(file_path: &Path, pattern: &String) {
+    // println!("{:?} - inside search file", file_path.display());
     let file = match File::open(file_path) {
         Ok(file) => file,
         Err(_) => {
@@ -53,7 +84,6 @@ fn search_file(file_path: &Path, pattern: String) {
 
     let reader = BufReader::new(file);
 
-    println!("{:>8}  {}", "Line No.".yellow(), "Text".yellow());
     for (line_no, line_result) in reader.lines().enumerate() {
         let line = match line_result {
             Ok(line) => line,
@@ -67,8 +97,13 @@ fn search_file(file_path: &Path, pattern: String) {
         }
         let lower_line = line.to_lowercase();
 
-        if lower_line.contains(&pattern) {
-            println!("{:>8}  {}", line_no.to_string().red(), line.green());
+        if lower_line.contains(pattern) {
+            println!(
+                "{:>20} {:>8}  {}",
+                file_path.display(),
+                line_no.to_string().red(),
+                line.green()
+            );
         }
     }
 }
