3ac319bba18e3838df9c792117914e608a16bf05
yashraj-pradip-haridas <yashraj.haridas22@pccoepune.org>
2026-03-22 16:06:14 +0530

    completed the basic setup for getting the data from the client to server and print the data

--- Personal_projects/tcp_server/src/main.rs
+++ Personal_projects/tcp_server/src/main.rs
@@ -1,3 +1,29 @@
+use std::{
+    io::{BufRead, BufReader},
+    net::{TcpListener, TcpStream},
+};
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        println!("Connection Established");
+
+        let _ = handle_client(stream);
+    }
+}
+
+fn handle_client(stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+    let mut buff = BufReader::new(stream);
+
+    loop {
+        let mut body_buffer = String::new();
+        let data = buff.read_line(&mut body_buffer)?;
+        println!("{}", body_buffer.trim());
+        if data <= 0 {
+            break;
+        }
+    }
+    println!("{}", "Client Disconnected");
+    Ok(())
 }
