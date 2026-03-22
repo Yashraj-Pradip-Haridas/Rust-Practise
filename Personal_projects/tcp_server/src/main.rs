5ee6d1c99773af43a4442b9716d84014e34430b0
yashraj-pradip-haridas <yashraj.haridas22@pccoepune.org>
2026-03-22 16:33:33 +0530

    added multithreading to all multiple client to connect to the server

--- Personal_projects/tcp_server/src/main.rs
+++ Personal_projects/tcp_server/src/main.rs
@@ -1,15 +1,17 @@
 use std::{
     io::{BufRead, BufReader},
     net::{TcpListener, TcpStream},
+    thread,
 };
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        println!("Connection Established");
-
-        let _ = handle_client(stream);
+        thread::spawn(move || {
+            println!("Connection Established");
+            let _ = handle_client(stream);
+        });
     }
 }
 
@@ -20,7 +22,7 @@ fn handle_client(stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
         let mut body_buffer = String::new();
         let data = buff.read_line(&mut body_buffer)?;
         println!("{}", body_buffer.trim());
-        if data <= 0 {
+        if data == 0 {
             break;
         }
     }
