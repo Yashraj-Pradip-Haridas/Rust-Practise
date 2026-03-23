dfc113faeccd8b779b4a2ddf61f8b4f465cc639b
yashraj-pradip-haridas <yashraj.haridas22@pccoepune.org>
2026-03-24 00:16:29 +0530

    completed the mvp with get set and del commands

--- Personal_projects/mini-redis/src/main.rs
+++ Personal_projects/mini-redis/src/main.rs
@@ -35,11 +35,21 @@ fn handle_client(stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
         if data == 0 {
             break;
         }
+        if body_buffer == "EXIT".trim().to_string() {
+            break;
+        }
 
-        let command: Vec<&str> = body_buffer.split_whitespace().collect();
         // println!("{:?}", command);
 
-        let _ = handle_commands(command, &stream);
+        match handle_commands(body_buffer, &stream) {
+            Ok(_) => {
+                println!("Command successful");
+            }
+            Err(e) => {
+                println!("{}", e);
+                continue;
+            }
+        };
     }
     println!("{}", "Client Disconnected");
 
@@ -47,22 +57,53 @@ fn handle_client(stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
 }
 
 fn handle_commands(
-    command: Vec<&str>,
+    command: String,
     mut stream: &TcpStream,
 ) -> Result<(), Box<dyn std::error::Error>> {
+    let command: Vec<&str> = command.split_whitespace().collect();
+    if command.len() < 1 {
+        return Err("No command found".into());
+    }
     match command[0].trim() {
         "GET" => {
+            if command.len() < 2 || command[1].trim().is_empty() {
+                return Err("Key not found".into());
+            }
             println!("Entered the get function");
-            let out_value = get_key_value_pair(command[1]);
+            let mut out_value = get_key_value_pair(command[1]);
+            if out_value.is_empty() {
+                out_value = "(nil)".to_string();
+            }
             println!(" Value: {}", out_value);
-            let response = out_value + "\n";
-            stream.write_all(response.as_bytes())?;
+            stream.write_all((out_value + "\n").as_bytes())?;
         }
         "SET" => {
             println!("Entered the set function");
+            if command.len() < 3 || command[1].trim().is_empty() || command[2].trim().is_empty() {
+                return Err("Key/Value not found".into());
+            }
             set_key_value_pair(command[1], command[2]);
+            stream.write_all(String::from("Ok\n").as_bytes())?;
         }
-        _ => {}
+        "DEL" => {
+            if command.len() < 2 || command[1].trim().is_empty() {
+                return Err("Key not found".into());
+            }
+            match del_key_value_pair(command[1]) {
+                Some(val) => {
+                    println!("Removed value: {}", val);
+                    stream.write_all((val + "\n").as_bytes())?;
+                }
+                None => {
+                    println!("No value found");
+                    stream.write_all("(nil)\n".as_bytes())?;
+                }
+            };
+        }
+        _ => {
+            stream.write_all("Invalid command \n".as_bytes())?;
+            return Err("Invalid command".into());
+        }
     }
     Ok(())
 }
@@ -81,4 +122,9 @@ fn get_key_value_pair(key: &str) -> String {
         None => return String::new(),
     };
     value
+}
+
+fn del_key_value_pair(key: &str) -> Option<String> {
+    let mut data = SHARED_DATA.lock().unwrap();
+    data.remove(key)
 }
